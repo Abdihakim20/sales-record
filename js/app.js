58641502e3a57b8751e557a4c6405b7b547112b4
Abdihakim <h.abdihakim01@gmail.com>
2021-11-03 14:23:39 +0300

    modified app js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.getElementById('main').addEventListener('submit', addItems)
+const products = document.getElementById('product')
+const quantity = document.getElementById('quantity-number')
+const selling = document.getElementById('selling-price')
+const totalAmount = document.getElementById('total-amount')
+
+function addItems(e) {
+    e.preventDefault()
+
+}
